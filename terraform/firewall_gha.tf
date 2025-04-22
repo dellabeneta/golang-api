d0d1fb003c7d9e70041641e99b19d267cb5867e2
dellabeneta <m.dellabeneta@gmail.com>
2025-04-22 19:59:33 -0300

    ajuste terraform

--- terraform/firewall_gha.tf
+++ terraform/firewall_gha.tf
@@ -1,24 +1,24 @@
 
-resource "digitalocean_firewall" "firewall_mongo" {
-  name = "firewall-mongo"
+resource "digitalocean_firewall" "firewall_gha" {
+  name = "firewall-gha"
 
-  droplet_ids = [digitalocean_droplet.mongodb.id]
+  droplet_ids = [digitalocean_droplet.gha.id]
 
   inbound_rule {
     protocol         = "tcp"
     port_range       = "1-65535"
-    source_addresses = ["10.0.20.0/24"]
+    source_addresses = ["170.247.104.231/32"]
   }
 
   inbound_rule {
     protocol         = "udp"
     port_range       = "1-65535"
-    source_addresses = ["10.0.20.0/24"]
+    source_addresses = ["170.247.104.231/32"]
   }
 
   inbound_rule {
     protocol         = "icmp"
-    source_addresses = ["10.0.20.0/24"]
+    source_addresses = ["170.247.104.231/32"]
   }
 
   outbound_rule {
